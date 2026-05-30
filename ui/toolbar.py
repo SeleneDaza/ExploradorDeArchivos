1acc22b1dbb862840a641f94233a864a12697a10
Liceth Lopez <liceth.lopez02@uptc.edu.co>
2026-05-29 21:26:25 -0500

    Fase 3 - toolbar actualizado

--- ui/toolbar.py
+++ ui/toolbar.py
@@ -3,7 +3,7 @@ from PyQt5.QtGui import QFont
 
 
 class Toolbar(QWidget):
-    def __init__(self, on_up, on_home, on_root):
+    def __init__(self, on_up, on_home, on_root, on_new_folder, on_new_file):
         super().__init__()
         self.setFixedHeight(42)
 
@@ -11,16 +11,20 @@ class Toolbar(QWidget):
         layout.setContentsMargins(8, 4, 8, 4)
         layout.setSpacing(6)
 
-        btn_up   = QPushButton("⬆  Subir")
-        btn_home = QPushButton("🏠  Home")
-        btn_root = QPushButton("💻  Raíz")
+        btn_up         = QPushButton("⬆  Subir")
+        btn_home       = QPushButton("🏠  Home")
+        btn_root       = QPushButton("💻  Raíz")
+        btn_new_folder = QPushButton("📁  Nueva carpeta")
+        btn_new_file   = QPushButton("📄  Nuevo archivo")
 
-        for btn in [btn_up, btn_home, btn_root]:
+        for btn in [btn_up, btn_home, btn_root, btn_new_folder, btn_new_file]:
             btn.setFixedHeight(30)
 
         btn_up.clicked.connect(on_up)
         btn_home.clicked.connect(on_home)
         btn_root.clicked.connect(on_root)
+        btn_new_folder.clicked.connect(on_new_folder)
+        btn_new_file.clicked.connect(on_new_file)
 
         self.path_label = QLabel()
         self.path_label.setFont(QFont("Monospace", 9))
@@ -28,6 +32,8 @@ class Toolbar(QWidget):
         layout.addWidget(btn_up)
         layout.addWidget(btn_home)
         layout.addWidget(btn_root)
+        layout.addWidget(btn_new_folder)
+        layout.addWidget(btn_new_file)
         layout.addSpacing(12)
         layout.addWidget(self.path_label, stretch=1)
 
